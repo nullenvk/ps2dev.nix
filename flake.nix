3ce9bc90b096962bd33f6d5587a828daf979b06d
nullenvk <nullenvk@protonmail.com>
2025-05-04 20:38:19 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "PS2 homebrew development toolchain";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+
+      ];
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        packages = {
+          dvp-binutils = pkgs.callPackage ./pkgs/dvp-binutils {}; 
+          iop-binutils = pkgs.callPackage ./pkgs/iop-binutils {};
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [ 
+            self'.packages.dvp-binutils
+          ];
+        };
+      };
+      flake = {
+
+      };
+    };
+}
---
+++ pkgs/dvp-binutils/default.nix
@@ -0,0 +1,38 @@
+{stdenv, pkgs}:
+stdenv.mkDerivation {
+    pname = "dvp-binutils";
+    version = "2.44.0";
+
+    src = pkgs.fetchFromGitHub {
+        owner = "ps2dev";
+        repo = "binutils-gdb";
+        rev = "0aef5ed1686ba47069392798a5d4fd03d183bf8a"; # dvp-v2.44.0 branch
+        hash = "sha256-gKldknLyP22v+b5nynrJwY0THj84dQD39/b4THhoayI=";
+    };
+
+    buildInputs = [
+        pkgs.gcc
+        pkgs.gmp
+        pkgs.libmpc
+        pkgs.mpfr
+        pkgs.texinfo
+        pkgs.gnumake
+        pkgs.flex
+        pkgs.bison
+    ];
+
+    patches = [
+        ./deterministic.patch
+    ];
+
+    configureFlags = [
+        "--target=dvp"
+        "--disable-nls"
+        "--disable-build-warnings"
+    ];
+
+    dontUpdateAutotoolsGnuConfigScripts = true;
+    dontStrip = true; # is it necessary?
+    dontDisableStatic = true;
+    hardeningDisable = ["all"];
+}
